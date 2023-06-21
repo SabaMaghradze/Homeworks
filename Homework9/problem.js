a201f6212df08b364cf0546c28be160874a96d1e
SabaMaghradze <sabamaghradze1001@gmail.com>
2023-06-21 21:59:39 +0400

    whatever

--- Homework9/problem.js
+++ Homework9/problem.js
@@ -4,8 +4,6 @@
 
 let url = 'https://jsonplaceholder.typicode.com/users';
 
-let forDOM;
-
 fetch(url)
   .then(response => {
     if (response.ok == false) {
@@ -14,14 +12,29 @@ fetch(url)
     return response.json();
   })
   .then(users => {
-    forDOM = users;
+    display(users[0]);
   })
   .catch(error => {
     console.log('Error:', 'An error occurred');
   });
 
-setTimeout(() => {
-    console.log(forDOM);
-}, 3000);
+const usersDiv = document.createElement('div');
+usersDiv.setAttribute('class', 'usersDiv');
+document.body.appendChild(usersDiv);
+
+let userDiv;
+
+
+
+function display(info) {
+  for (let i = 0; i < info.length; i++) {
+    userDiv = document.createElement('div');
+    userDiv.setAttribute('class', 'userDiv');
+    userDiv.innerText = info[i];
+    usersDiv.appendChild(userDiv);
+  }
+}
+
+console.log(usersDiv);
 
 //   დასრულება ვერ მოვასწარი
